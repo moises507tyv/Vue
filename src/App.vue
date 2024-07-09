6ec79c21a21792ca9536a988b37f853d4a59eb5b
moises507tyv <ascenciotejedaurbano@gmail.com>
2024-07-08 18:17:19 -0600

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import SizeSelector from './components/SizeSelector.vue';
+
+const selectedSize = ref<String>('M');
+
+</script>
+
+<template>
+ <h1>Seleccione la talla: {{selectedSize}} </h1>
+ <SizeSelector
+ :selected-size="selectedSize"
+ @on-changed-size="selectedSize = $event"
+ 
+ />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+button{
+  padding: 5px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  width: 75px;
+  cursor: pointer;
+
+}
+.active{
+  background-color: black;
+  color:white;
+  transition: all .3s ease-out ;
+
+}
+</style>
---
+++ src/components/SizeSelector.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const possibleSizes = ['XS','S','M','L','XL','XXL'];
+
+
+// javaScript
+//const props = defineProps({
+  //  selectedSize: { type: String, required: true }
+
+//});
+
+//const emits = defineEmits ({
+    //onChanceSize: String
+//});
+
+
+// typeScript
+
+const props = defineProps<{
+    selectedSize: String
+
+}>();
+
+const emits = defineEmits<{
+  ( event: 'onChangedSize' , value: String): void,
+}>();
+
+</script>
+
+<template>
+   
+
+    <button 
+v-for="size in possibleSizes"
+:class="{ 'active': props.selectedSize === size }"
+@click="emits('onChangedSize', size)"
+>
+
+
+
+{{size}}
+
+</button>
+
+</template>
